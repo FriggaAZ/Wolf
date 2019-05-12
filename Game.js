8583c2eabb2cb65c4f8c9d40d64f87bbcecca890
FriggaAZ <FriggaAZ@outlook.com>
2019-05-12 16:21:58 +0800

    让🐺随机出现在洞口处

--- Game.js
+++ Game.js
@@ -1,23 +1,23 @@
-class Game{
-	constructor(id) {
-	    //获得画布
-		let canvas = document.getElementById(id);
-		//获得画笔
-		this.paint = canvas.getContext('2d');
-		
-		//创建游戏运行页面
-		this.gameView = new GameView();
-	}
-	
-	run(){
-		console.log("游戏运行");
-		this.gameView.run(this.paint);
-	}
-	
-	start(){
-		//启动线程    每200毫秒调用当前的run方法
-		setInterval(this.run.bind(this),200);
-	}
-	
-	
+class Game {
+    constructor(id) {
+        //获得画布
+        let canvas = document.getElementById(id);
+        //获得画笔
+        this.paint = canvas.getContext('2d');
+
+        //创建游戏运行页面
+        this.gameView = new GameView();
+    }
+
+    run() {
+        console.log("游戏运行");
+        this.gameView.run(this.paint);
+    }
+
+    start() {
+        //启动线程    每200毫秒调用当前的run方法
+        setInterval(this.run.bind(this), 200);
+    }
+
+
 }
--- Wolf.js
+++ Wolf.js
@@ -1,5 +1,20 @@
 class Wolf {
     constructor() {
+        this.arrPos = [
+            { x: 170, y: 245 },
+            { x: 50, y: 320 },
+            { x: 45, y: 420 },
+            { x: 70, y: 540 },
+            { x: 200, y: 510 },
+            { x: 330, y: 540 },
+            { x: 320, y: 406 },
+            { x: 305, y: 287 },
+            { x: 200, y: 510 }
+        ]
+
+        // this.getIndexPos();
+        this.indexPos = Math.floor(Math.random() * this.arrPos.length);
+
         let imgDataH = [
             './img/wolf/h0.png',
             './img/wolf/h1.png',
@@ -25,6 +40,9 @@ class Wolf {
     }
 
 
+    getIndexPos() {
+        this.indexPos = Math.floor(Math.random() * this.arrPos.length);
+    }
 
     run(paint) {
         //业务逻辑
@@ -35,12 +53,17 @@ class Wolf {
 
     logic() {
         this.index++;
-        if (this.index >= 9)
+        if (this.index >= 6) {
             this.index = 0;
+
+            //随机当前位置
+            this.getIndexPos();
+        }
+
     }
 
     onDraw(paint) {
         //绘制地图
-        paint.drawImage(this.imgHs[this.index], this.imgX, this.imgY, this.imgW, this.imgH);
+        paint.drawImage(this.imgHs[this.index], this.arrPos[this.indexPos].x, this.arrPos[this.indexPos].y, this.imgW, this.imgH);
     }
 }
